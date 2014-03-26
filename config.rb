516f74a21081e46cedd55c79e33549215dd71287
Bogardo <chrisbogaards@gmail.com>
2014-03-26 02:17:33 +0100

    Added config.rb for default SASS/Compass configuration

---
+++ config.rb
@@ -0,0 +1,14 @@
+# Directory Paths
+css_dir = "public/assets/css"
+sass_dir = "public/assets/scss"
+images_dir = "public/assets/img"
+javascripts_dir = "public/assets/js"
+
+# Preferred Syntax. Can be :scss or :sass. Defaults to :scss.
+preferred_syntax = :scss
+
+# The output style for the compiled css. One of: :nested, :expanded, :compact, or :compressed.
+output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
